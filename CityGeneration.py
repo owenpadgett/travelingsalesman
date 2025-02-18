77083c0ff4b679db06f7d53532b7bc8b686accee
owenpadgett <owen.padgett@uconn.edu>
2025-02-18 01:56:40 +0000

    added more descriptive docstring to generate_cities_specific

--- CityGeneration.py
+++ CityGeneration.py
@@ -7,7 +7,10 @@ def generate_cities(num_cities, randomize=True, seed=42):
     return [(random.randint(0, 100), random.randint(0, 100)) for _ in range(num_cities)]
 
 def generate_cities_specific(num_cities, range=((0, 10),(0, 10)), seed=None):
-    """Generate city coordinates. num_cities: integer, range=tuple of ((xmin, xmax)(ymin, ymax)), seed if specified"""
+    """Generate city coordinates. 
+    num_cities: integer: number of cities desired
+    range: tuple of ((xmin, xmax)(ymin, ymax)): defines bounds that cities can generate in
+    seed if specified"""
     if seed:
         random.seed(seed)  # Ensure predictable placement
     return [(random.randint(range[0][0], range[0][0]), random.randint(range[1][0], range[1][1])) for _ in range(num_cities)]
