ec8794e929705becb2d6f1e14c63ea732df19800
owenpadgett <owen.padgett@uconn.edu>
2025-02-19 00:04:43 +0000

    peantiz

--- DistanceFormulas.py
+++ DistanceFormulas.py
@@ -1,6 +1,8 @@
 import math
 import numpy as np
 import CityGeneration as cg
+import itertools
+import PathTools as pt
 
 def dist2(city1, city2=(0,0)):
     """Euclidean distance or l2 norm between two cities in R^2:
@@ -14,11 +16,20 @@ def dist3(city1, city2=(0,0,0)):
     Output: float: Distance between two cities."""
     return math.sqrt((city1[0]-city2[0])**2 + (city1[1]-city2[1])**2 + (city1[2]-city2[2])**2)
 
-def path_dist2(path):
+def depr_path_dist2(path):
     """Returns the distance traveled over the length of a path.
     Input: List of ordered pairs: List of city coordinates, in order.
     Output: float: Distance along the path."""
     return sum(dist2(path[i], path[i + 1]) for i in range(len(path) - 1))
+
+def eff_path_dist2(path, dist_matrix):
+    """Returns the distance traveled over the length of a path.
+    Input: List of tuples (index: int, tuples (ordered pair of coordinates)): Named list of cities.
+    Output: float: Distance along the path."""
+    sum = 0
+    for i in range(len(path)-1):
+        sum += dist_matrix[i][i+1]
+    return sum
 
 def dist_matrix2(city_list):
     """Returns a symmetric matrix representing the distance between each city.
@@ -37,3 +48,41 @@ def dist_matrix2(city_list):
                 dist_matrix[i][j] = dist2(city_list[i], city_list[j])
     
     return np.add(dist_matrix, dist_matrix.transpose())
+
+def eff_brute_force_shortest_path(city_list, dist_matrix):
+    """Find the shortest path using brute force.
+    Input: List of ordererd pairs: List of city coordinates.
+    Output: tuple (list of ordered pairs, float)."""
+    min_distance = float('inf')
+    best_path = None
+    for perm in itertools.permutations(city_list):
+        dist = eff_path_dist2(perm, dist_matrix)
+        if dist < min_distance:
+            min_distance = dist
+            best_path = pt.pathNorm(perm)
+    
+    return best_path, min_distance
+
+def _shortest_path(city_list):
+        """Find the shortest path using brute force."""
+        min_distance = float('inf')
+        best_path = None
+    
+        for perm in itertools.permutations(city_list):
+            normalized_perm = perm
+            dist = depr_path_dist2(normalized_perm)
+            if dist < min_distance:
+                min_distance = dist
+                best_path = normalized_perm
+    
+        return best_path, min_distance
+
+def centroid2(city_list):
+    """Finds the 'centroid' or center of mass of all the cities.
+    Input: List of ordered pairs: List of city coordinates.
+    Output: Ordered pair: Coordinates of the centroid."""
+    ctr = (0,0)
+    for i in city_list:
+        ctr[0] += i[0]
+        ctr[1] += i[1]
+    return ctr/len(city_list)
---
+++ PathTools.py
@@ -0,0 +1,16 @@
+import DistanceFormulas as df
+
+
+def pathNorm(path):
+    """Returns the non-cyclical path normalized, so that reversed paths are considered equivalent.
+    Input: List of ordered pairs: List of cities visited.
+    Output: List of ordered pairs: List of cities visited (normalized)."""
+    rev = path[::-1]
+    if path[0][0] > rev[0][0]:
+        return rev
+    elif path[0][0] == rev[0][0]:
+        if path[0][1] > rev[0][1]:
+            return rev
+        else: return path
+    else: return path
+
